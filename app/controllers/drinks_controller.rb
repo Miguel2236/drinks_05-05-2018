7120d636904391c59804be4c0c6248adea7f6895
Miguel2236 <miguelm2236@gmail.com>
2018-05-05 16:41:43 -0500

    login

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,4 +1,5 @@
 class DrinksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_drink, only: [:show, :edit, :update, :destroy]
 
   # GET /drinks
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :drinks do
   	member do
   		post :upvote
